07ea51fdefc3a23f310126ffd96c97a8de1198f5
TehhX <splashyfish100@hotmail.com>
2025-03-30 16:41:21 -0400

    Empty Window

---
+++ EmptyWindow/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+int main() {
+    // Initializing GLFW:
+    if (!glfwInit()) {
+        std::cerr << "Could not initialize GLFW.\n";
+        return 1;
+    }
+
+    // Window details:
+    constexpr int width             { 1000 };        // Width of the window.
+    constexpr int height            { 1000 };        // Height of the window.
+    constexpr char windowName[]     { "My Window" }; // Name of the window.
+    GLFWmonitor* const monitor      { nullptr };     // Unsure.
+    GLFWwindow*  const prevWindow   { nullptr };     // Unsure.
+
+    // Creating a window and getting its handle:
+    GLFWwindow* const window { glfwCreateWindow(width, height, windowName, monitor, prevWindow) };
+    if (window == nullptr) {
+        std::cerr << "Could not create window.\n";
+        return 1;
+    }
+
+    // Unsure what this does, has something to do with OpenGL's state maybe:
+    glfwMakeContextCurrent(window);
+
+    // Message loop:
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW, free resources:
+    glfwTerminate();
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Learning GLFW
 A repo to learn the GLFW library.
+
+## Repo Structure
+The root folder contains the gitignore, README, and license files, along with several folders that have their own projects and CMake files within them. All of the folders are their own application, completely seperate from one another, and teach a different thing in GLFW, e.g. EmptyWindow shows the most basic GLFW program, an empty window.
