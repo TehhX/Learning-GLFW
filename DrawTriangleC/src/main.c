cfe76aeb2ea9b68f6c37e351d7e0fe431cc58b0c
Sam <156055395+TehhX@users.noreply.github.com>
2025-09-17 12:46:16 -0400

    Updated DrawTriangleC more. Will continue work on it.

--- DrawTriangleC/src/main.c
+++ DrawTriangleC/src/main.c
@@ -12,10 +12,15 @@ int main(int argc, char **argv) {
     if (!glfwInit())
         return 1;
 
+    // Specify GLFW version and profile. This was made in version 3.3, and only uses modern functionality. Because of this, compatability is *not* required, and core is used.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // Make windows created after this expression unresizable
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    // Create a window and assign window its handle.
+    // Create a window and assign window its handle. The fourth argument (monitor) specifies the monitor if exclusive fullscreen is desired, NULL if windowed. The last argument (share) is for sharing resources between windows, and is not required.
     GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "DrawTriangle", NULL, NULL);
     if (!window)
         return 1;
@@ -32,18 +37,14 @@ int main(int argc, char **argv) {
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
-    // Vertex buffer stuff
-    const float triangleVerts[] = {
+    // Screen location of each of the triangle's vertices. The screen starts at (-1, -1, 0) at the top left, and ends at (1, 1, 0) bottom right.
+    GLfloat triangle_vertices[] = {
         -0.5f, -0.7f, 0.0f,
          0.5f, -0.7f, 0.0f,
          0.0f,  0.7f, 0.0f
     };
 
-    GLuint VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVerts), triangleVerts, GL_STATIC_DRAW);
-
+    // Vertex shader, creates the locations of the vertices.
     const char *vertexShaderSource =
         "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
@@ -55,16 +56,7 @@ int main(int argc, char **argv) {
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
-    GLint success;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if (!success) {
-        char infoLog[INFO_SIZE];
-        glGetShaderInfoLog(vertexShader, INFO_SIZE, NULL, infoLog);
-        puts(infoLog);
-        return 1;
-    }
-
+    // Fragment shader, creates the colour of the vertices.
     const char *fragmentShaderSource =
         "#version 330 core\n"
         "out vec4 FragColor;\n"
@@ -77,50 +69,60 @@ int main(int argc, char **argv) {
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
+    // Shader program 
     GLuint shaderProgram = glCreateProgram();
-
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    // Deletes the shaders used in creating the program, as they have been used and are not required anymore.
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
+    // Makes sure the shader program was a success.
+    GLint success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         char infoLog[INFO_SIZE];
-        glGetShaderInfoLog(shaderProgram, INFO_SIZE, NULL, infoLog);
+        glGetShaderInfoLog(vertexShader, INFO_SIZE, NULL, infoLog);
         puts(infoLog);
         return 1;
     }
 
+    GLuint vertex_buffer_object, // Is used to transmit a large buffer of data to the GPU at once.
+           vertex_array_object;  // Stores pointers to (n >= 1) vertex_buffer_object's and tells OpenGL how to use them.
+
+    glGenVertexArrays(1, &vertex_array_object);
+    glGenBuffers(1, &vertex_buffer_object);
+
+    glBindVertexArray(vertex_array_object);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Sets background colour.
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
     glUseProgram(shaderProgram);
+    glBindVertexArray(vertex_array_object);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    // Buffers are switched between frames so as to not flicker the screen by drawing data directly onto the screen. The above lines are applied to the unused buffer. This swaps the unused buffer with the used one.
+    glfwSwapBuffers(window);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    GLuint VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVerts), triangleVerts, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glfwSwapBuffers(window);
+    while (!glfwWindowShouldClose(window))
         glfwPollEvents();
-    }
+
+    glDeleteVertexArrays(1, &vertex_array_object);
+    glDeleteBuffers(1, &vertex_buffer_object);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
 }
