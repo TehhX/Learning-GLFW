42ff3e04e6c42e3a54656734499d4d0096d96cd5
TehhX <156055395+TehhX@users.noreply.github.com>
2025-10-08 18:02:33 -0400

    Continued Transformations work, confirmed BASE... changes to work on Linux.

--- Transformations/src/main.c
+++ Transformations/src/main.c
@@ -2,9 +2,30 @@
 #include "GLFW/glfw3.h"
 #include "cglm/cglm.h"
 
-#include "stdio.h"
-
 int main()
 {
-    puts("Hello, world!");
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow *win = glfwCreateWindow(800, 800, "Transformations", 0, 0);
+    glfwMakeContextCurrent(win);
+    glfwSwapInterval(1);
+
+    gladLoadGL();
+    glClearColor(0.2, 0.2, 0.2, 1);
+
+    while (!glfwWindowShouldClose(win))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(win);
+    glfwTerminate();
 }
