a5ff78d6532d8f300fd8e10c3aec0b4b0db4bba3
TehhX <156055395+TehhX@users.noreply.github.com>
2025-11-04 22:24:34 -0500

    Fixed issue with DrawCircle, added some more options.

--- DrawCircle/src/main.c
+++ DrawCircle/src/main.c
@@ -4,22 +4,26 @@
 #include "stdio.h"
 #include "math.h"
 
-#define WIN_SIDE_LEN 1000 // Side length of square window
-#define ORIGIN_X 0.0 // Center of the circle (X)
-#define ORIGIN_Y 0.0 // Center of the circle (Y)
-#define RADIUS 0.75 // How large the circle is in relation to the window [0, 1].
-#define CIRCLE_VERT_COUNT 32 // Make higher for better looking circle.
+// VSCode thinks M_PI wasn't defined by math.h (which it was), this is to shut it up.
+#ifndef M_PI
+    #define M_PI 1234.5678
+#endif
+
+#define WIN_SIDE_LEN 800 // Side length of square window
+#define BACKGROUND_RGB 0.5, 0.25, 0.1
+
+#define ORIGIN_X -0.2 // Center of the circle (X)
+#define ORIGIN_Y 0.2 // Center of the circle (Y)
+#define RADIUS 0.8 // How large the circle is in relation to the window [0, 1].
+
+#define CIRCLE_VERT_COUNT 128 // Amount of outer vertices in the circle, one at the origin will always exist in addition.
+#define DRAW_TYPE 0 // Either 1 for fill or 0 for wireframe
+#define DRAW_INDIVIDUAL -1 // -1 for all, otherwise [0, CIRCLE_VERT_COUNT)
 
 typedef GLuint GLref;
 
 int main()
 {
-    if (sizeof(float) != sizeof(GLfloat))
-    {
-        puts("Float size mismatch.");
-        return 1;
-    }
-
     if (glfwInit() == GLFW_FALSE)
     {
         puts("Could not init.");
@@ -53,24 +57,6 @@ int main()
 
     glViewport(0, 0, WIN_SIDE_LEN, WIN_SIDE_LEN);
 
-    float circle_verts[CIRCLE_VERT_COUNT * 2 + 2] = { ORIGIN_X, ORIGIN_Y };
-    for (int i = 0; i < CIRCLE_VERT_COUNT; ++i)
-    {
-        // https://www.desmos.com/calculator/ru8sg90siu
-        circle_verts[0 + 2 * (i + 1)] = cos(2 * M_PI / CIRCLE_VERT_COUNT * i) * RADIUS + ORIGIN_X;
-        circle_verts[1 + 2 * (i + 1)] = sin(2 * M_PI / CIRCLE_VERT_COUNT * i) * RADIUS + ORIGIN_Y;
-    }
-
-    GLuint circle_indices[3 * CIRCLE_VERT_COUNT + 3] = { 0 };
-    for (int i = 0; i < CIRCLE_VERT_COUNT; ++i)
-    {
-        circle_indices[1 + i * 3] = i + 1;
-        circle_indices[2 + i * 3] = i + 2;
-    }
-
-    circle_indices[1 + CIRCLE_VERT_COUNT * 3] = CIRCLE_VERT_COUNT;
-    circle_indices[2 + CIRCLE_VERT_COUNT * 3] = 1;
-
     GLref vbo, vao, ebo;
     glGenBuffers(1, &vbo);
     glGenVertexArrays(1, &vao);
@@ -80,10 +66,37 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(circle_verts), circle_verts, GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(circle_indices), circle_indices, GL_STATIC_DRAW);
+    {
+        GLfloat circle_verts[(CIRCLE_VERT_COUNT + 1) * 2];
+            circle_verts[0] = ORIGIN_X;
+            circle_verts[1] = ORIGIN_Y;
+        for (int i = 1; i <= CIRCLE_VERT_COUNT; ++i)
+        {
+            // https://www.desmos.com/calculator/ru8sg90siu
+            circle_verts[0 + 2 * i] = cos(2 * M_PI / CIRCLE_VERT_COUNT * i) * RADIUS + ORIGIN_X;
+            circle_verts[1 + 2 * i] = sin(2 * M_PI / CIRCLE_VERT_COUNT * i) * RADIUS + ORIGIN_Y;
+        }
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void *) 0);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(circle_verts), circle_verts, GL_STATIC_DRAW);
+    }
+
+    {
+        GLuint circle_indices[3 * CIRCLE_VERT_COUNT];
+        for (int i = 0; i < CIRCLE_VERT_COUNT - 1; ++i)
+        {
+            circle_indices[0 + i * 3] = 0;
+            circle_indices[1 + i * 3] = i + 1;
+            circle_indices[2 + i * 3] = i + 2;
+        }
+
+        circle_indices[0 + (CIRCLE_VERT_COUNT - 1) * 3] = 0;
+        circle_indices[1 + (CIRCLE_VERT_COUNT - 1) * 3] = CIRCLE_VERT_COUNT;
+        circle_indices[2 + (CIRCLE_VERT_COUNT - 1) * 3] = 1;
+
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(circle_indices), circle_indices, GL_STATIC_DRAW);
+    }
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2, (void *) 0);
     glEnableVertexAttribArray(0);
 
     GLref shader_prog = glCreateProgram();
@@ -118,12 +131,12 @@ int main()
 
     glUseProgram(shader_prog);
 
-    glClearColor(0.1, 0.1, 0.1, 1.0);
+    glClearColor(BACKGROUND_RGB, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    // Second argument GL_LINE for wireframe circle, GL_FILL for filled circle.
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(DRAW_TYPE ? GL_FRONT : GL_FRONT_AND_BACK, DRAW_TYPE ? GL_FILL : GL_LINE);
 
-    glDrawElements(GL_TRIANGLES, sizeof(circle_indices) / sizeof(GLuint), GL_UNSIGNED_INT, (void *) 0);
+    glDrawElements(GL_TRIANGLES, DRAW_INDIVIDUAL >= 0 ? 3 : 3 * CIRCLE_VERT_COUNT, GL_UNSIGNED_INT, (void *) (sizeof(GLuint) * 3 * (DRAW_INDIVIDUAL >= 0 ? DRAW_INDIVIDUAL : 0)));
 
     glfwSwapBuffers(main_window);
 
